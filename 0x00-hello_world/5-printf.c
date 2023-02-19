94bb2d555debef557f5156a677e14be61e2d92ef
nelsonhano <koinyannelson0@gmail.com>
2023-02-18 21:17:36 -0800

    s-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,3 +8,12 @@ printf("And its form is as perfect as can be.\n");
 printf("Truly, a work of genuis, don't you agree?\n");
 return (0);
 }
+/**
+ * main - A program that print a line using the printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n")
+return (0);
+}
