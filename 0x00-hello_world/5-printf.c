8c41b6353067f4e2b0bf9dd47b4a5a25b4f8669e
nelsonhano <koinyannelson0@gmail.com>
2023-02-18 21:08:43 -0800

    s-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/**
- * main - A program that print a line using the printf function
- * Return: 0 (Success)
- */
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art,\n")
+printf("Behold, a masterpiece of art,\n");
+printf("Whose beauty is matched by no other part.\n");
+printf("Its colors and line are a sight to see,\n");
+printf("And its form is as perfect as can be.\n");
+printf("Truly, a work of genuis, don't you agree?\n");
 return (0);
 }
